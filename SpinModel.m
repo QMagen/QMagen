f5f1f0eb946ab5d5a11fbef9961ec723bbe56425
Yuan Gao <17231064@buaa.edu.cn>
2021-03-14 14:21:33 +0800

    TL-XXZ

--- SpinModel.m
+++ SpinModel.m
@@ -79,18 +79,44 @@ switch Conf.ModelName
         Conf.IntrcMap_Name = 'IntrcMap_TLXXZ';
         Conf.ModelName_all = 'Triangular lattice-XXZ model';
         Conf.d = 2;
-        ModelConf.Para_List = {'J1', 'J2', 'Delta'};
+        ModelConf.Para_List = {'J1xy', 'Delta1', 'J2xy', 'Delta2'};
         ModelConf.Para_Range = cell(length(ModelConf.Para_List), 1);
-        ModelConf.Num_gFactor = 1;                           
+        ModelConf.Num_gFactor = 2;                           
         ModelConf.Type_gFactor = 'xyz';      
         ModelConf.gFactor = cell(ModelConf.Num_gFactor, 1);       
         ModelConf.gFactor_Vec = cell(ModelConf.Num_gFactor, 1);   
         ModelConf.gFactor_Range = cell(ModelConf.Num_gFactor, 1); 
-        ModelConf.gFactor{1} = 'gz';
-        ModelConf.gFactor_Vec{1} = [0,0,1];
+        ModelConf.gFactor{1} = 'gx';
+        ModelConf.gFactor_Vec{1} = [1,0,0];
+        ModelConf.gFactor{2} = 'gz';
+        ModelConf.gFactor_Vec{2} = [0,0,1];
+        
+        % =================================================================
+        % GEOMETRY SETTINGS
+        % =================================================================
+        GeomConf.Lx = 3;                                   
+        GeomConf.Ly = 3;
+        GeomConf.BCX = 'OBC';
+        GeomConf.BCY = 'PBC';
+        Conf.L = GeomConf.Lx * GeomConf.Ly;
+        
+        % =================================================================
+        % PARAMETER SETTINGS
+        % =================================================================
+        % J1xy range
+        ModelConf.Para_Range{1} = [5, 20];  
+        % Delta1 range
+        ModelConf.Para_Range{2} = 'J1xy';  
+        % J2xy range
+        ModelConf.Para_Range{3} = [0, 20];  
+        % Delta2 range
+        ModelConf.Para_Range{4} = 'J2xy';  
+        % gx range
+        ModelConf.gFactor_Range{1} = 'gz'; 
+        % gz range
+        ModelConf.gFactor_Range{2} = [1.5, 3];
     otherwise
-        fprintf('1!\n')
-        keyboard;
+        error('Undefined model name!\n')
         
 end
 end

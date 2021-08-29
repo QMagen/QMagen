5e27d0b83f6ec1da9482b2ba6fdc599afb51604c
wei li <w.li@buaa.edu.cn>
2021-08-30 00:34:06 +0800

    XXZ_test modification

--- PlotScript/plot_Landscape.m
+++ PlotScript/plot_Landscape.m
@@ -1,2 +1,22 @@
+
+% ========= Plot Landscape in the parameter space ===== %
+% Usage:
+% (i) LandscapePlot(res, 'Para1', 'Para2', 'native/log', 'FigDim', '2D/3D')
+% (ii) LandscapePlot(res, 'Para1', 'Para2', 'native/log', 'FigDim', '2D/3D', CrossSectionPoint)
+
+% Input parameters: 
+% "Para1", "Para2" are names of two parameters
+% "native" or "log": native loss L or log10(L), [c.f. LossConf.Design in RunOpt_XX.m]
+% 'FigDim', '2D' or '3D'
+% 'CrossSectionPoint' - cross section through point [X1, X2, ...] [c.f.
+% related model files in SpinMode folder]
+
+% QMagen Collaboration: YG.BUAA & WL.ITP, 2021-08-30
+% ====================================================== %
+
+% load the result file (stored in "/Tmp/")
+% load ../Tmp/tmp_20210829_213241/res3.mat
 load res.mat
-[bs] = LandscapePlot(res, 'J1', 'J2', 'native', 'FigDim', '2D');
+
+% //Plot the crosssection 
+[bs] = LandscapePlot(res, 'Jxy', 'Jz', 'log', 'FigDim', '2D');

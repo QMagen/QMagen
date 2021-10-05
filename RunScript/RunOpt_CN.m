cecb07d27f8a6157ab2d61e0bb44c2d540c10bd4
Yuan Gao <17231064@buaa.edu.cn>
2021-10-06 00:04:27 +0800

    *

---
+++ RunScript/RunOpt_CN.m
@@ -0,0 +1,141 @@
+
+% // ====================== Cu(NO3)2 2.5 H2O ==============================
+% ref: Xiang, et al. Sci. Rep. 7, 44643 (2017).
+%
+% Spin model [../SpinModel/SpinModel_HAFC]
+% Bayesian optimization parameters [ImportBOPara.m]
+% Many-body solver parameters [ImportMBSolverPara.m]
+% =========================================================================
+% QMagen Collaboration: YG.BUAA & WL.ITP, 2021-08-30
+
+clear all
+
+addpath('../Class')
+addpath('../Expdata')
+addpath('../LossFunc')
+addpath('../ManyBodySolver')
+addpath('../svd_lapack_interface')
+addpath('../Tmp')
+addpath('../UtilityFunc')
+
+addpath(genpath('../SpinModel'))
+maxNumCompThreads(4);
+
+% // ==================== User Input: Parameters ==========================
+Para.ManyBodySolver = 'iLTRG'; % 'ED', 'iLTRG', 'XTRG'
+Para.ModelName = 'HAFC';
+Para.Mode = 'OPT';
+
+% // import specific heat data --------------------------------------------
+% CmDataFile: experimental data file name include Data
+%             Data(:,1) -> temperarture   Unit: K
+%             Data(:,2) -> specifit heat  Unit: J/(mol K)
+% CmDataFile = {'FileName1'; 'FileName2'; ...};
+CmDataFile = {'../ExpData/CuN_C_expdata_0T.mat'; ...
+              '../ExpData/CuN_C_expdata_2.82T.mat'; ...
+              '../ExpData/CuN_C_expdata_3.57T.mat'};
+
+% CmDataTRange: the temperature range to be fitted (Unit: K)
+% CmDataTRange = {[T1l, T1u]; [T2l, T2u]; ...};
+CmDataTRange = {[0.3, 5]; ...
+                [0.3, 5]; ...
+                [0.5, 3]};
+
+% CmDataField: the magnetic filed of experimantal data (Unit: Tesla)
+% CmDataField = {[B1x, B1y, B1z]; [B2x, B2y, B2z]; ...};
+CmDataField = {[0,0,0]; ...
+               [0,0,2.82]; ...
+               [0,0,3.57]};
+
+% CmDatagInfo: the No. of g factor used for the conversion
+% Only require when MoldeConf.gFactor_Type = 'dir';
+% CmDatagInfo = {gNum1, gNum2, ...};
+CmDatagInfo = {};
+%--------------------------------------------------------------------------
+
+% // import susceptibility data -------------------------------------------
+% CmDataFile: experimental data file name include Data
+%             Data(:,1) -> temperarture   Unit: K
+%             Data(:,2) -> susceptibility Unit: cm^3/mol (1/4pi * emu/mol)
+% ChiDataFile = {'FileName1'; 'FileName2'; ...};
+ChiDataFile = {'../ExpData/CuN_Chi_expdata_Sz.mat'};
+
+% ChiDataTRange: the temperature range to be fitted (Unit: K)
+% ChiDataTRange = {[T1l, T1u]; [T2l, T2u]; ...};
+ChiDataTRange = {[1.5, 20]};
+
+% CmDataField: the magnetic filed of experimantal data (Unit: Tesla)
+% CmDataField = {[B1x, B1y, B1z]; [B2x, B2y, B2z]; ...};
+ChiDataField = {[0,0,0.1]};
+
+% ChiDatagInfo: the No. of g factor used for the conversion
+% Only require when MoldeConf.gFactor_Type = 'dir';
+% ChiDatagInfo = {gNum1, gNum2, ...};
+ChiDatagInfo = {};
+%--------------------------------------------------------------------------
+
+% // loss function --------------------------------------------------------
+% the weights between different data
+% LossConf.WeightList = [], equal weight
+% LossConf.WeightList = [w1, w2, ...];
+LossConf.WeightList = [];
+
+% type of loss function 
+%       'abs-err' absolute error, normalized by maximum value
+%       'rel-err' relative error
+LossConf.Type = 'abs-err'; % 'abs-err', 'rel-err'
+
+% design of loss function
+%       'log'     L = log10(L)
+%       'native'  L = L
+LossConf.Design = 'log'; % 'native', 'log'
+
+% Interpolation options when fitting
+%       'Int2Exp' Interpolation to experimental data
+%       'Int2Sim' Interpolation to simulation data
+LossConf.IntSet = 'Int2Exp';
+%--------------------------------------------------------------------------
+
+% // settings -------------------------------------------------------------
+% plot result in each iteration
+Setting.PLOTFLAG = 0; % 0 -> off, 1 -> on
+
+% save intermediate results.
+Setting.SAVEFLAG = 1;   % 0 -> off, 1 -> save the best, 2 -> save all
+
+% the file name to save intermediate results.
+Setting.SAVENAME = 'CuN';
+%--------------------------------------------------------------------------
+
+% // restart --------------------------------------------------------------
+
+Restart.XTrace = [];
+Restart.ObjTrace = [];
+Restart.Resume = [];
+%--------------------------------------------------------------------------
+% =========================================================================
+% // ====================== Package input data ============================
+[ Lattice, ModelConf, Para ] = GetSpinModel( Para );
+
+for i = 1:1:length(CmDataFile)
+    CmData(i) = ThermoData('Cm', CmDataField{i}, CmDataTRange{i}, CmDataFile{i});
+    
+    if strcmp(ModelConf.gFactor_Type, 'dir')
+        CmData(i).Info.g_info = CmDatagInfo{i};
+    end
+end
+
+for i = 1:1:length(ChiDataFile)
+    ChiData(i) = ThermoData('Chi', ChiDataField{i}, ChiDataTRange{i}, ChiDataFile{i});
+    
+    if strcmp(ModelConf.gFactor_Type, 'dir')
+        ChiData(i).Info.g_info = ChiDatagInfo{i};
+    end
+end
+
+Para.BOPara = ImportBOPara(Para.ManyBodySolver);
+QMagenConf = QMagen(Para, ModelConf, Lattice, LossConf, Setting, 'Restart', Restart, 'Cm', CmData, 'Chi', ChiData);
+% =========================================================================
+
+% // main function of QMagen
+QMagenMain(QMagenConf)
---
+++ RunScript/RunThDQ_CN.m
@@ -0,0 +1,110 @@
+addpath('../Class/');
+addpath('../ExpData/');
+addpath('../LossFunc/');
+addpath('../ManyBodySolver/');
+addpath(genpath('../SpinModel'));
+addpath(genpath('../svd_lapack_interface'));
+addpath('../UtilityFunc/');
+% maxNumCompThreads(16);
+
+% // ==================== User Input: Parameters ==========================
+Para.ManyBodySolver = 'iLTRG'; % 'ED', 'iLTRG', 'XTRG'
+Para.ModelName = 'HAFC';
+% Para.Mode = 'LOSS';
+Para.Mode = 'ThDQ';
+% // import specific heat data --------------------------------------------
+
+
+% CmDataTRange: the temperature range to be fitted (Unit: K)
+% CmDataTRange = {[T1l, T1u]; [T2l, T2u]; ...};
+CmDataTRange = {[0.1, 10]; [0.1, 10]; [0.1, 10]; [0.1, 10]; [0.1, 10]; [0.1, 10]};
+
+
+% CmDataField: the magnetic filed of experimantal data (Unit: Tesla)
+% CmDataField = {[B1x, B1y, B1z]; [B2x, B2y, B2z]; ...};
+CmDataField = {[0,0,0]; [0,0,1]; [0,0,2]; [0,0,3]; [0,0,4]; [0,0,5]};
+
+% CmDatagInfo: the No. of g factor used for the conversion
+% Only require when MoldeConf.gFactor_Type = 'dir';
+% CmDatagInfo = {gNum1, gNum2, ...};
+CmDatagInfo = {};
+%--------------------------------------------------------------------------
+
+% // import susceptibility data -------------------------------------------
+ChiDataTRange = {[0.1,90]};
+ChiDataField = {[0,0,0.1]};
+ChiDatagInfo = {};
+
+% ============================ %
+% Loss function customization
+% ============================ %
+% the weights between different data
+% LossConf.WeightList = [], equal weight
+% LossConf.WeightList = [w1, w2, ...];
+LossConf.WeightList = [];
+
+% type of loss function 
+%       'abs-err' absolute error, normalized by maximum value
+%       'rel-err' relative error
+LossConf.Type = 'abs-err'; % 'abs-err', 'rel-err'
+
+% design of loss function
+%       'log'     L = log10(L)
+%       'native'  L = L
+LossConf.Design = 'log'; % 'native', 'log'
+
+% Interpolation options when fitting
+%       'Int2Exp' Interpolation to experimental data
+%       'Int2Sim' Interpolation to simulation data
+LossConf.IntSet = 'Int2Exp';
+% ====================== %
+% Save & Plot Settings
+% ====================== %
+% plot result in each iteration
+Setting.PLOTFLAG = 0; % 0 -> off, n -> plot the graph every n iterations
+
+% save intermediate results.
+Setting.SAVEFLAG = 1;   % 0 -> off, 1 -> save the best, 2 -> save all
+
+% the file name to save intermediate results.
+Setting.SAVENAME = 'CN';
+
+
+% // restart --------------------------------------------------------------
+
+% load('../Tmp/edtest/tmp_20210604_194838/res5.mat')
+Restart.XTrace = [];
+Restart.ObjTrace = [];
+Restart.Resume = [];
+
+% // ====================== Package input data ============================
+[ Lattice, ModelConf, Para ] = GetSpinModel( Para );
+
+for i = 1:1:length(CmDataField)
+    CmData(i) = ThermoData('Cm', CmDataField{i}, CmDataTRange{i});
+    
+    if strcmp(ModelConf.gFactor_Type, 'dir')
+        CmData(i).Info.g_info = CmDatagInfo{i};
+    end
+end
+
+for i = 1:1:length(ChiDataField)
+    ChiData(i) = ThermoData('Chi', ChiDataField{i}, ChiDataTRange{i});
+    
+    if strcmp(ModelConf.gFactor_Type, 'dir')
+        ChiData(i).Info.g_info = ChiDatagInfo{i};
+    end
+end
+
+Para.BOPara = ImportBOPara(Para.ManyBodySolver);
+QMagenConf = QMagen(Para, ModelConf, Lattice, LossConf, Setting,'Restart',Restart, 'Cm', CmData, 'Chi', ChiData);
+%only calculate Cm
+%QMagenConf = QMagen(Para, ModelConf, Lattice, LossConf, Setting, 'Restart', Restart, 'Cm',  CmData);
+%only calculate Chi
+%QMagenConf = QMagen(Para, ModelConf, Lattice, LossConf, Setting, 'Chi', ChiData);
+
+% =========================================================================
+% // main function of QMagen
+
+[RsltCvNU, RsltCv, RsltChiNU, RsltChi] = QMagenMain(QMagenConf, 'J', 5.13, 'Delta', 1, 'alpha', 0.23, 'gz', 2.31);
+
